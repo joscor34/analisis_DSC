479cb628dc99d5681964389de36257f2ddf315ad
joscor34 <zbrm34@gmail.com>
2021-09-07 23:33:21 -0500

    Se agrego un programa para obtener los bordes de la imagen

---
+++ python_version/getting_edges.py
@@ -0,0 +1,12 @@
+# import the required libraries
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+# %matplotlib inline
+# read the image
+image = cv2.imread('../assets/Curva_DSC.png')
+# calculate the edges using Canny edge algorithm
+edges = cv2.Canny(image, 100, 200)
+# plot the edges
+cv2.imshow('Canny Edge Detection', edges)
+cv2.waitKey(0)
